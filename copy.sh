4844732a6f1eeafd3b1c4c6141d933538fa49cb7
Finn Hediger <finn.hediger@thurweb.ch>
2020-11-28 16:09:16 +0100

    Move start.sh and copy.sh to root

--- copy.sh
+++ copy.sh
@@ -5,11 +5,14 @@ root_path="/var/www/nesabot"
 echo -n "Using the root_path $root_path, continue? (y/n) " && read REPLY
 if [[ "$REPLY" =~ (y|Y|yes|Yes) ]]; then
     ssh server "bash -s" < "$(pwd)/scripts/set_permissions.sh"
+
     scp -r "$(pwd)/src" server:$root_path/src
     scp -r "$(pwd)/scripts" server:$root_path/scripts
+    scp -r "$(pwd)/start.sh" server:$root_path/start.sh
     scp "$(pwd)/dockerfile" server:$root_path/dockerfile
     scp "$(pwd)/requirements.txt" server:$root_path/requirements.txt
     scp "$(pwd)/nesabot.service" server:$root_path/nesabot.service
+
     ssh server "$root_path/scripts/install.sh"
 else
     echo "Aborting ..."
