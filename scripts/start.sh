234e718c76be7aa5f9bd2e98f19b537b0559239e
Finn Hediger <finn.hediger@thurweb.ch>
2020-11-28 11:48:50 +0100

    Fix volume path

--- scripts/start.sh
+++ scripts/start.sh
@@ -2,7 +2,7 @@
 
 if [ "$1" =~ "(l|local)" ]; then
     echo "Starting on local test machine ..."
-    docker run -v "$(pwd)/src":/nesabot/src local/nesabot
+    docker run -v "/mnt/hgfs$(pwd)/src":/nesabot/src local/nesabot
 else
     echo "Starting on remote linux server ..."
     docer run -v "/var/www/nesabot/src":/nesabot/src local/nesabot
